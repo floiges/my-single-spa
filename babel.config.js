af4443692e8cb080b8d457f0b0f56a0eac61b0f9
刘亚东 <liuyadong@yunkezan.com.cn>
2019-12-24 11:55:37 +0800

    feat: init project

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+// 配置 babel
+module.exports = function (api) {
+  // 缓存 babel 的配置
+  api.cache(true); // 等同于 api.cache.forever();
+  return {
+    presets: [
+      ['@babel/preset-env', { module: false }]
+    ],
+    plugins: ['@babel/plugin-syntax-dynamic-import']
+  };
+};
